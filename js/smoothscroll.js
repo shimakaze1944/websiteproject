ff041379dfac4d10cbbebd855efcf4d1da80b231
Alfindi Abdillah Farizky <81307080+archamanromania@users.noreply.github.com>
2022-01-08 15:10:55 +0700

    scrolling update

---
+++ js/smoothscroll.js
@@ -0,0 +1,73 @@
+/* Smooth Scrolling*/
+$("#sec-1").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#home").offset().top - 100,
+    },
+    1000
+  );
+  return false;
+});
+
+$("#sec-2").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#about").offset().top - 200,
+    },
+    1000
+  );
+  return false;
+});
+
+$("#sec-3").click(function () {
+  $(" html,body").animate(
+    {
+      scrollTop: $("#service").offset().top - 220,
+    },
+    1000
+  );
+  return false;
+});
+
+$("#sec-4").click(function () {
+  $(this).addClass("active");
+  $("html,body ").animate(
+    {
+      scrollTop: $("#quote").offset().top - 120,
+    },
+    1000
+  );
+  return false;
+});
+
+$("#about").waypoint(
+  function () {
+    $(".container ul li").children().removeClass("active");
+    $("#sec-2").addClass("active");
+  },
+  { offset: 101 }
+);
+
+$("#service").waypoint(
+  function () {
+    $(".container ul li").children().removeClass("active");
+    $("#sec-3").addClass("active");
+  },
+  { offset: 101 }
+);
+
+$("#quote").waypoint(
+  function () {
+    $(".container ul li").children().removeClass("active");
+    $("#sec-4").addClass("active");
+  },
+  { offset: 101 }
+);
+
+$("#home").waypoint(
+  function () {
+    $(".container ul li").children().removeClass("active");
+    $("#sec-1").addClass("active");
+  },
+  { offset: 0 }
+);
